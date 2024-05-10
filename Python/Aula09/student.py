12e299e24d79e4819a693f668e06904b316ff13d
Matheus Cunha <matheusrsr.cunha@gmail.com>
2024-05-10 18:02:57 -0300

    Atualizando para criando as propriedaeds na classe par avalidação

--- Python/Aula09/student.py
+++ Python/Aula09/student.py
@@ -1,5 +1,16 @@
 class Student:
-    ...
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house 
+    
+    @property
+    def houses(self):
+        return self._house
+    
+    @houses.setter
+    def house(self, house):
+        if house not in ["Gryffindor", "HuffLepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("INvalid house in Hogwarts")
 
 def main():
 
@@ -8,10 +19,10 @@ def main():
     print(f"{student.name} from {student.house}")
 
 def get_student():
-    student = Student()
+    name = input("Name: ")
+    house = input("House: ")
 
-    student.name = input("Name: ")
-    student.house = input("House: ")
+    student = Student(name, house) # Construtor de usando uma classe de modelo.
 
     return student    
 
