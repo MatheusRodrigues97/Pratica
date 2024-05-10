4ee3561749e4fa0d1abf0d9d7b3389184560b70d
Matheus Cunha <matheusrsr.cunha@gmail.com>
2024-05-10 16:01:28 -0300

    Atualizando para crianção da classe do estudante

--- Python/Aula09/student.py
+++ Python/Aula09/student.py
@@ -1,4 +1,19 @@
-name = input("Name: ")
-house = input("house: ")
+class Student:
+    ...
 
-print(f"{name} from {house}")
+def main():
+
+    student = get_student()
+
+    print(f"{student.name} from {student.house}")
+
+def get_student():
+    student = Student()
+
+    student.name = input("Name: ")
+    student.house = input("House: ")
+
+    return student    
+
+if __name__ == "__main__":
+    main()
