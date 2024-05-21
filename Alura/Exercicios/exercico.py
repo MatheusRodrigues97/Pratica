12cff717cf316eaef15de24731fecad9bb462a8c
Matheus Cunha <matheusrsr.cunha@gmail.com>
2024-05-21 09:49:55 -0300

    feito o segundo exercico pratico

--- Alura/Exercicios/exercico.py
+++ Alura/Exercicios/exercico.py
@@ -2,3 +2,11 @@
 # Imprima a frase "pyhton na escola de programação alura"
 
 print("Python na escola de programação da Alura\n")
+
+#Exercico 02
+# Imprima a frade Meu nome é {nome} e tenho {idade} anos
+
+nome = 'Matheus Rodrigues'
+idade = 27
+
+print(f'Meu nome é {nome} e tenho {idade} anos\n')
