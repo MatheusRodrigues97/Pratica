12b782eca5a9057bad2843ade8b51e921e284b10
Matheus Cunha <matheusrsr.cunha@gmail.com>
2024-06-28 11:39:01 -0300

    Feita a alteração do codigo para realizar a postagem das imagens no site e ja ficar online

--- Alura/Django/alura-space/galeria/admin.py
+++ Alura/Django/alura-space/galeria/admin.py
@@ -6,6 +6,7 @@ class ListandoFotografias(admin.ModelAdmin):
     list_display = ('id', 'nome', 'publicada')
     list_display_links = ('id', 'nome')
     list_filter = ('categoria',)
+    search_fields = ('nome',)
     list_editable = ('publicada',)
     list_per_page = 20
 
